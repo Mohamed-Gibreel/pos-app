67dd7e492b8bcb34ef344c850b60f4aa7e74f1d7
Mohamed Gibreel <m.adilgibreel@gmail.com>
2025-09-14 16:34:58 +0400

    fix: use logical unit for line height

--- lib/utils/styles/flutter_extenstions.dart
+++ lib/utils/styles/flutter_extenstions.dart
@@ -1,14 +1,19 @@
 import 'package:base_template/utils/styles/app_colors.dart';
 import 'package:base_template/utils/styles/app_colors_extension.dart';
+import 'package:base_template/utils/styles/app_text_extenstion.dart';
 import 'package:flutter/material.dart';
 
 extension AppThemeExtension on ThemeData {
   AppColorsExtension get colors =>
       extension<AppColorsExtension>() ?? AppColors.lightAppColors;
+
+  AppTextThemeExtension get textStyle =>
+      extension<AppTextThemeExtension>() ?? AppColors.textTheme;
 }
 
 extension ThemeGetter on BuildContext {
   AppColorsExtension get appColor => Theme.of(this).colors;
+  AppTextThemeExtension get textStyle => Theme.of(this).textStyle;
   // Usage example: `context.theme`
   ThemeData get theme => Theme.of(this);
 }
